1202e186bb2920becb44a7f0f15dd5f8759fd41f
shobanyogi <shobanyogi1996@gmail.com>
2017-04-18 11:10:44 +0530

    Add files via upload

---
+++ roman.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main()
+{
+ 
+char a;
+ 
+printf("Enter the character");
+ 
+scanf("%c",&a);
+ 
+switch(a)
+ {
+     
+case 'x':
+     
+printf("10");
+     
+break;
+     
+case 'I':
+     
+printf("1");
+     
+break;
+     
+case 'v':
+     
+printf("5");
+     
+break;
+ }
+ 
+return 0;
+}
