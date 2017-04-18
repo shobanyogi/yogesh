a71c6228dfe8b6982910accf7ef14a95b4eaf621
shobanyogi <shobanyogi1996@gmail.com>
2017-04-18 11:59:06 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+char a[20];
+    
+int i,j,temp;
+    
+printf("Enter the charcter\n");
+    
+scanf("%s",a);
+   
+for(i=0;i<strlen(a);i=i+2)
+   
+{
+       
+temp=a[i];
+       
+a[i]=a[i+1];
+     
+ a[i+1]=temp;}      
+printf("%s",a);
+   
+return 0;
+}
